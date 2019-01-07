495bbee8e98455d4356f419d2b44c669e4909de4
FBeaumont <flbeaumo@student.42.fr>
2019-01-07 17:55:18 +0100

    all test passed

--- test_GNL/get_next_line.h
+++ test_GNL/get_next_line.h
@@ -6,15 +6,15 @@
 /*   By: florian <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 21:33:31 by florian           #+#    #+#             */
-/*   Updated: 2019/01/07 17:35:15 by flbeaumo         ###   ########.fr       */
+/*   Updated: 2019/01/07 16:49:55 by flbeaumo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 9 
-# define MAX_FD 4864
+# define BUFF_SIZE 1
+# define MAX_FD 1023
 # include "libft/libft.h"
 # include <unistd.h>
 # include <fcntl.h>
--- test_GNL/gnl_work_without_norm.c
+++ test_GNL/gnl_work_without_norm.c
@@ -1,15 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   gnl_work_without_norm.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: flbeaumo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/13 19:52:30 by flbeaumo          #+#    #+#             */
-/*   Updated: 2019/01/07 17:34:24 by flbeaumo         ###   ########.fr       */
+/*   Created: 2018/12/07 14:36:37 by flbeaumo          #+#    #+#             */
+/*   Updated: 2019/01/07 16:46:26 by flbeaumo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+ *====== V 1 ======*
+ *   Norm Fucked   *
+ *=================*
+ */
 #include "get_next_line.h"
 
 int			cut_line(char **s, char **line)
@@ -17,15 +22,17 @@ int			cut_line(char **s, char **line)
 	char	*tmp;
 	int		len;
 
+	len = ft_strclen(*s, '\n');
 	if (ft_strchr(*s, '\n'))
 	{
-		len = ft_strclen(*s, '\n');
 		if (!(*line = ft_strsub(*s, 0, len)))
 			return (-1);
 		if (!(tmp = ft_strdup(*s + len + 1)))
 			return (-1);
 		free(*s);
 		*s = tmp;
+		if (s[0] == '\0')
+			ft_strdel(s);
 	}
 	else
 	{
@@ -36,30 +43,13 @@ int			cut_line(char **s, char **line)
 	return (1);
 }
 
-int			read_files(int fd, char **s, int ret)
-{
-	char		buff[BUFF_SIZE + 1];
-	char		*tmp;
-
-	while ((ret = read(fd, &buff, BUFF_SIZE)) > 0)
-	{
-		buff[ret] = '\0';
-		if (!(tmp = ft_strjoin(*s, buff)))
-			return (-1);
-		free(*s);
-		*s = tmp;
-		if (ft_strchr(buff, '\n'))
-			break ;
-	}
-	return (ret);
-}
-
 int			get_next_line(int fd, char **line)
 {
 	static char	*s[MAX_FD];
+	char		buff[BUFF_SIZE + 1];
 	int			ret;
+	char		*tmp;
 
-	ret = 1;
 	if (line == NULL || fd < 0)
 		return (-1);
 	if (!s[fd])
@@ -67,11 +57,22 @@ int			get_next_line(int fd, char **line)
 			return (-1);
 	if (ft_strchr(s[fd], '\n'))
 		return (cut_line(&s[fd], line));
-	else
-		ret = read_files(fd, &s[fd], ret);
+	else 
+	{
+		while ((ret = read(fd, &buff, BUFF_SIZE)) > 0)
+		{
+			buff[ret] = '\0';
+			if (!(tmp = ft_strjoin(s[fd], buff)))
+				return (-1);
+			free(s[fd]);
+			s[fd] = tmp;
+			if (ft_strchr(buff, '\n'))
+				return (cut_line(&s[fd], line));
+		}
+	}
 	if (ret < 0)
 		return (-1);
-	if (ret == 0 && (s[fd][0] == '\0'))
+	if ((!s[fd] && (ret == 0)) || s[fd][0] == '\0')
 		return (0);
 	return (cut_line(&s[fd], line));
 }
