644f1b5d13be95b1523fc5f4b067f805ae8675d7
FBeaumont <flbeaumo@student.42.fr>
2019-01-07 14:38:04 +0100

    retry 6 fails + work without norm

--- gnl_work_without_norm.c
+++ gnl_work_without_norm.c
@@ -1,15 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   gnl_work_without_norm.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: flbeaumo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/13 19:52:30 by flbeaumo          #+#    #+#             */
-/*   Updated: 2019/01/07 14:37:26 by flbeaumo         ###   ########.fr       */
+/*   Created: 2018/12/07 14:36:37 by flbeaumo          #+#    #+#             */
+/*   Updated: 2019/01/07 14:37:19 by flbeaumo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+ *====== V 1 ======*
+ *   Norm Fucked   *
+ *=================*
+ */
 #include "get_next_line.h"
 
 int			cut_line(char **s, char **line)
@@ -26,36 +31,25 @@ int			cut_line(char **s, char **line)
 			return (-1);
 		free(*s);
 		*s = tmp;
+		if (s[0] == '\0')
+			ft_strdel(s);
 	}
-	if (s[0] == '\0')
-		ft_strdel(s);
-	return (1);
-}
-
-int			read_files(int fd, char **s, int ret)
-{
-	char		buff[BUFF_SIZE + 1];
-	char		*tmp;
-
-	while ((ret = read(fd, &buff, BUFF_SIZE)) > 0)
+	else
 	{
-		buff[ret] = '\0';
-		if (!(tmp = ft_strjoin(*s, buff)))
+		if (!(*line = ft_strdup(*s)))
 			return (-1);
-		free(*s);
-		*s = tmp;
-		if (ft_strchr(buff, '\n'))
-			break ;
+		ft_strdel(s);
 	}
-	return (ret);
+	return (1);
 }
 
 int			get_next_line(int fd, char **line)
 {
 	static char	*s[MAX_FD];
+	char		buff[BUFF_SIZE + 1];
 	int			ret;
+	char		*tmp;
 
-	ret = 0;
 	if (line == NULL || fd < 0)
 		return (-1);
 	if (!s[fd])
@@ -63,12 +57,22 @@ int			get_next_line(int fd, char **line)
 			return (-1);
 	if (ft_strchr(s[fd], '\n'))
 		return (cut_line(&s[fd], line));
-	else
-		ret = read_files(fd, &s[fd], ret);
+	else 
+	{
+		while ((ret = read(fd, &buff, BUFF_SIZE)) > 0)
+		{
+			buff[ret] = '\0';
+			if (!(tmp = ft_strjoin(s[fd], buff)))
+				return (-1);
+			free(s[fd]);
+			s[fd] = tmp;
+			if (ft_strchr(buff, '\n'))
+				return (cut_line(&s[fd], line));
+		}
+	}
 	if (ret < 0)
 		return (-1);
-	return (ret == 0 ? 0 : cut_line(&s[fd], line));
+	if ((!s[fd] && (ret == 0)) || s[fd][0] == '\0')
+		return (0);
+	return (cut_line(&s[fd], line));
 }
-
-
-/*
