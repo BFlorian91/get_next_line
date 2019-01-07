0c582fbc1d4d926f65503ea47192f082938d25de
FBeaumont <flbeaumo@student.42.fr>
2019-01-07 18:02:03 +0100

    all test passed

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: florian <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 21:33:31 by florian           #+#    #+#             */
-/*   Updated: 2019/01/07 17:35:15 by flbeaumo         ###   ########.fr       */
+/*   Updated: 2019/01/07 18:01:44 by flbeaumo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 9 
+# define BUFF_SIZE 9
 # define MAX_FD 4864
 # include "libft/libft.h"
 # include <unistd.h>
